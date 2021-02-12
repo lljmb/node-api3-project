d4e571a9f8844c66df6cb3a47260f0dba438663b
lljmb <brown.jessm@icloud.com>
2021-02-12 02:43:52 -0500

    i will not be defeated

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require('cors');
+
 
 const usersRouter = require('./users/users-router');
 const postsRouter = require('./posts/posts-router');
@@ -10,25 +9,22 @@ const mw = require('./middleware/middleware');
 
 const server = express();
 
-const publicPath = path.join(__dirname, '..', 'public')
-
 // remember express by default cannot parse JSON in request bodies
 server.use(helmet());
-server.use(cors())
 server.use(express.json(), morgan('dev'));
-server.use(express.static(publicPath))
 
 // global middlewares and routes need to be connected here
 server.use(mw.logger);
 server.use('/api/users', usersRouter);
 server.use('/api/posts', postsRouter)
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
-server.get('*', (res, req) => {
-  res.sendFile(path.join(publicPath, 'index.html'))
+server.use('/', (_, res) => {
+  res.status(400).json({message: 'i pray this works'})
 })
+
+// server.get('/', (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
+
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 // require your server and launch it
-const server = require('./api/server')
+const server = require('./api/server.js')
 
-const port = process.env.PORT || 1213;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`server listening on ${port}`)
