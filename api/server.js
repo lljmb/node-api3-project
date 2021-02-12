2c4d66c341d55a39a02e5f5559a56b44050095ec
lljmb <brown.jessm@icloud.com>
2021-02-12 00:23:23 -0500

    still unbreaking

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -9,11 +10,13 @@ const mw = require('./middleware/middleware');
 
 const server = express();
 
+const publicPath = path.join(__dirname, '..', 'public')
 
 // remember express by default cannot parse JSON in request bodies
 server.use(helmet());
 server.use(cors())
 server.use(express.json(), morgan('dev'));
+server.use(express.static(publicPath))
 
 // global middlewares and routes need to be connected here
 server.use(mw.logger);
@@ -23,5 +26,9 @@ server.use('/api/posts', postsRouter)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.get('*', (res, req) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
 
 module.exports = server;
